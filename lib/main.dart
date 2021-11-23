f01206e8b92b260ed5d0537db164010a95130741
shsh <rypush@naver.com>
2021-11-23 17:23:29 +0900

    Getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 import 'Webview.dart';
 
 void main() {
@@ -11,7 +11,7 @@ class KigoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'KigoWebview',
       home: Webview(),
     );
